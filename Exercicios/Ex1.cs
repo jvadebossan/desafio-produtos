ee9a1592950b3877be18a4ddf1833b28213db903
João Debossan <joao.debossan@autoglass.com.br>
2025-03-14 12:59:04 -0300

    Adiciona exercício 1

--- Exercicios/Ex1.cs
+++ Exercicios/Ex1.cs
@@ -4,8 +4,13 @@ namespace DesafioProdutos.Exercicios;
 
 public class Ex1
 {
+    //TODO: Qual é o produto mais caro do estoque?
     public static void Exec()
     {
-        Console.WriteLine("teste");
+        var prodMaisCaro = Program.produtos.MaxBy(prod => prod.Preco);
+
+        Console.WriteLine($"O produto mais caro é o {prodMaisCaro.Nome}, custando {prodMaisCaro.Preco:c}.");
+
+        //RESPOSTA: O produto mais caro é o Para-choque Dianteiro Ford Fiesta, custando R$ 2.492,45.
     }
 }
