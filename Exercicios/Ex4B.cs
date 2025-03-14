625ca56770669ae8e0ce83044bc8bed6676b983c
João Debossan <joao.debossan@autoglass.com.br>
2025-03-14 13:37:00 -0300

    Adiciona exercício 4 B

---
+++ Exercicios/Ex4B.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace DesafioProdutos.Exercicios;
+
+public class Ex4B
+{
+    //TODO Crie um ranking dos 3 centros de distribuição com mais estoque. 
+    public static void Exec()
+    {
+        var estoque = Program.produtos.GroupBy(prod => prod.Empresa)
+        .Select(prod => new
+        {
+            nome = prod.Key,
+            quant = prod.Select(p => p.Quantidade).Sum()
+        })
+        .OrderByDescending(prod => prod.quant)
+        .Take(5);
+
+        int posicao = 1;
+        foreach (var prod in estoque)
+        {
+            Console.WriteLine($"{posicao}º Lugar: {prod.nome} - {prod.quant} unidades");
+            posicao++;
+        }
+        // 1º Lugar: Filial MG - 5449 unidades
+        // 2º Lugar: Filial SP - 5442 unidades
+        // 3º Lugar: Filial PR - 5222 unidades
+        // 4º Lugar: Filial RJ - 4738 unidades
+        // 5º Lugar: Filial RS - 4200 unidades
+    }
+}
--- Program.cs
+++ Program.cs
@@ -15,8 +15,8 @@ class Program
         //Ex1.Exec();
         //Ex2.Exec();
         //Ex3.Exec();
-        Ex4A.Exec();
-        //Ex4B.Exec();
+        //Ex4A.Exec();
+        Ex4B.Exec();
         //Ex4C.Exec();
         //Ex5.Exec();
         //Ex6.Exec();
