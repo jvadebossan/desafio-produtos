90caa770664cea75122e4d128e049273c4b94cfc
João Debossan <joao.debossan@autoglass.com.br>
2025-03-14 12:55:52 -0300

    Adiciona função para importar dados do excel

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using DesafioProdutos.Exercicios;
 using DesafioProdutos.Models;
+using NPOI.SS.UserModel;
 
 namespace DesafioProdutos;
 
@@ -10,6 +11,7 @@ class Program
 
     public static void Main(string[] args)
     {
+        ImportarExcel();
         Ex1.Exec();
         //Ex2.Exec();
         //Ex3.Exec();
@@ -18,5 +20,37 @@ class Program
         //Ex4C.Exec();
         //Ex5.Exec();
         //Ex6.Exec();
+    }
+
+    public static void ImportarExcel()
+    {
+        try
+        {
+            IWorkbook pastaTrabalho = WorkbookFactory.Create(caminhoArquivo);
+
+            ISheet planilha = pastaTrabalho.GetSheetAt(0);
+
+            for (int i = 1; i < planilha.PhysicalNumberOfRows; i++)
+            {
+                IRow linha = planilha.GetRow(i);
+
+                int codigo = (int)linha.GetCell(0).NumericCellValue;
+                string nome = linha.GetCell(1).StringCellValue;
+                string categoria = linha.GetCell(2).StringCellValue;
+                string fabricante = linha.GetCell(3).StringCellValue;
+                double preco = linha.GetCell(4).NumericCellValue;
+                int quantidade = (int)linha.GetCell(5).NumericCellValue;
+                DateTime dataEntrada = linha.GetCell(6).DateCellValue ?? DateTime.Now;
+                string empresa = linha.GetCell(7).StringCellValue;
+
+                var musica = new Produto(codigo, nome, categoria, fabricante, preco, quantidade, dataEntrada, empresa);
+
+                produtos.Add(musica);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
     }
 }
